9d5f7fec61ee7a27f740f6a197c959ad5797c9d7
QWolfPlaY <st30943@gmail.com>
2023-11-14 20:55:43 +0100

    Create structs for data

--- src/bin_generator/bin_generator.h
+++ src/bin_generator/bin_generator.h
@@ -7,7 +7,22 @@
 
 
 class bin_generator {
+public:
+    typedef struct {
+        unsigned int customerCount;
+        unsigned int workTime;
+        unsigned int cigaretteBreakLen;
+        unsigned char byteData[12] = {};
+    } header;
+    typedef struct {
+        unsigned int entryTime;
+        unsigned int timeUsage;
+    } customer;
 
+    static header generateHeader();
+    static customer generateCustomer();
+
+    static char* data();
 };
 
 
