af369bd717d320212576acd21de5dcbe1bf2895d
QWolfPlaY <st30943@gmail.com>
2023-12-13 22:01:46 +0100

    Implement new decoder fucntions

--- src/decoder/decoder.cpp
+++ src/decoder/decoder.cpp
@@ -6,32 +6,74 @@
 #include "bin_generator.h"
 #include "cpp-core/core.h"
 
-bin_generator::header decoder::headerFromBytes(char *headerByteData)
+bin_generator::header decoder::headerFromBytes(char* headerByteData)
 {
     bin_generator::header header;
 
-    header.workTime =
+    memcpy(&header.customerCount, headerByteData, sizeof(bin_generator::header().customerCount));
+    memcpy(&header.workTime,
+           headerByteData + sizeof(bin_generator::header().customerCount),
+           sizeof(bin_generator::header().workTime)
+           );
+    memcpy(&header.workTime,
+           headerByteData + sizeof(bin_generator::header().customerCount) + sizeof(bin_generator::header().workTime),
+           sizeof(bin_generator::header().cigaretteBreakLen)
+           );
 
-    return bin_generator::header();
+    return header;
 }
 
-bin_generator::customer decoder::customerFromBytes(char *customerByteData)
+bin_generator::customer decoder::customerFromBytes(char* customersByteData, size_t offset, const bool BYTE_OFFSET = true)
 {
-    return bin_generator::customer();
+    bin_generator::customer customer;
+
+    if (!BYTE_OFFSET) {
+        offset *= bin_generator::CUSTOMER_SIZE;
+    }
+
+    memcpy(&customer.entryTime, customersByteData + offset, sizeof(bin_generator::customer().entryTime));
+    memcpy(&customer.entryTime,
+           customersByteData + offset + sizeof(bin_generator::customer().entryTime),
+           sizeof(bin_generator::customer().timeUsage)
+           );
+
+    return customer;
+
 }
 
-bool decoder::verifyByteData(char *headerByteData, char *customerByteData, size_t fileSize)
+bool decoder::verifyByteFile(char* headerByteData, size_t fileSize)
 {
-    bin_generator::header header =
-    return false;
+    bin_generator::header header = headerFromBytes(headerByteData);
+
+    size_t totalCustomerByteSize = header.customerCount * bin_generator::CUSTOMER_SIZE;
+
+    if ((fileSize - bin_generator::HEADER_SIZE) < totalCustomerByteSize) { return false; }
+    else { return true; }
 }
 
-bool decoder::verifyRawData(bin_generator::header headerRawData, bin_generator::customer *customersRawData)
+bool decoder::verifyRawData(bin_generator::header header, std::vector<bin_generator::customer> customers)
 {
-    return false;
+    unsigned int customersTimeSum = 0;
+
+    for (bin_generator::customer customer : customers) {
+        customersTimeSum += customer.entryTime;
+        customersTimeSum += customer.timeUsage;
+    }
+
+    bool isTotalTimeCorrect = true;
+    bool isCustomerCountCorrect = true;
+    bool isCigaretteBreakLenCorrect = true;
+
+    if (customersTimeSum > header.workTime) { isTotalTimeCorrect = false; }
+    else if (customers.size() != header.customerCount) { isCustomerCountCorrect = false; }
+    else if (header.cigaretteBreakLen > header.workTime) { isCigaretteBreakLenCorrect = false; }
+
+    if (isTotalTimeCorrect && isCustomerCountCorrect && isCigaretteBreakLenCorrect) { return true; }
+    else { return false; }
 }
 
-unsigned int decoder::calculateBreaks(bin_generator::header headerRawData, bin_generator::customer *customersRawData)
+unsigned int decoder::calculateBreaks(bin_generator::header headerRawData, bin_generator::customer* customersRawData)
 {
+    
     return 0;
 }
--- src/decoder/decoder.h
+++ src/decoder/decoder.h
@@ -6,14 +6,15 @@
 #define KASJER_DECODER_H
 
 #include "bin_generator.h"
+#include "cpp-core/core.h"
 
 class decoder
 {
 public:
     static bin_generator::header headerFromBytes(char *headerByteData);
-    static bin_generator::customer customerFromBytes(char *customerByteData);
-    static bool verifyByteData(char *headerByteData, char *customerByteData, unsigned long long int fileSize);
-    static bool verifyRawData(bin_generator::header headerRawData, bin_generator::customer *customersRawData);
+    static bin_generator::customer customerFromBytes(char* customersByteData, size_t offset, const bool BYTE_OFFSET);
+    static bool verifyByteFile(char *headerByteData, size_t fileSize);
+    static bool verifyRawData(bin_generator::header headerRawData, std::vector<bin_generator::customer> customers);
     static unsigned int calculateBreaks(bin_generator::header headerRawData, bin_generator::customer *customersRawData);
 };
 
