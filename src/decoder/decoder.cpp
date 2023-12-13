381bf4f8ea83b304a376876f200cede27ce65ccb
QWolfPlaY <59415223+QWolfPlaY@users.noreply.github.com>
2023-12-13 18:47:00 +0100

    Implement decoder functions

--- src/decoder/decoder.cpp
+++ src/decoder/decoder.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "decoder.h"
+#include "bin_generator.h"
+#include "cpp-core/core.h"
+
+bin_generator::header decoder::headerFromBytes(char *headerByteData)
+{
+    bin_generator::header header;
+
+    header.workTime =
+
+    return bin_generator::header();
+}
+
+bin_generator::customer decoder::customerFromBytes(char *customerByteData)
+{
+    return bin_generator::customer();
+}
+
+bool decoder::verifyByteData(char *headerByteData, char *customerByteData, size_t fileSize)
+{
+    bin_generator::header header =
+    return false;
+}
+
+bool decoder::verifyRawData(bin_generator::header headerRawData, bin_generator::customer *customersRawData)
+{
+    return false;
+}
+
+unsigned int decoder::calculateBreaks(bin_generator::header headerRawData, bin_generator::customer *customersRawData)
+{
+    return 0;
+}
